a8b9079291a2d09245db668531bca28e87e9ee2c
Ruoyang Qiu <robinqiu1992@gmail.com>
2021-02-05 22:26:18 -0800

    Added IMealplanModel.ts to Interface

---
+++ src/Interfaces/IMealplanModel.ts
@@ -0,0 +1,22 @@
+import {ObjectId, Date, Document} from "mongoose"
+import {MealType} from '../Enums/MealType'
+import {CuisineType} from '../Enums/CuisineType'
+import {FeatureType} from '../Enums/FeatureType'
+import {AllergyType} from '../Enums/AllergyType'
+import {RecipeTag} from '../Enums/RecipeTag'
+
+
+interface IMealplanModel extends Document {
+    member_id: ObjectId,
+    date: Date;
+    budget: Number;
+    group: Number;
+    recipe_list: [ObjectId];
+    shopping_list: [ObjectId];
+    meal_type: MealType;
+    cuisine_type: CuisineType;
+    feature_type: FeatureType;
+    restrictions: [AllergyType];
+}
+
+export{IMealplanModel}
