22cedae8e9335755e4da31768a6fcd7ececfb987
HAO LI <haoli@HAOdeMacBook-Pro.local>
2021-02-08 02:56:58 -0800

    fixed error in app.ts

--- src/App.ts
+++ src/App.ts
@@ -2,12 +2,9 @@ import express, {Application, Request, Response} from "express";
 import * as bodyParser from 'body-parser';
 import {recipesRoute} from './Routes/Recipes';
 import {myRecipesRoute} from './Routes/MyRecipes';
-//import { userRoute } from "./Routes/User";
+import { userRoute } from "./Routes/User";
 
-import {UserModel} from './Models/UserModel'
 
-let userRoute = express.Router();
-let users = new UserModel();
 
 class App {
 
@@ -32,16 +29,6 @@ class App {
     // config API endpoints
     private setupRoutes(): void {
         
-        let router = express.Router();
-
-        router.get("/users/:uId", (req : Request, res : Response) => {
-            let id = req.params.uId;
-            console.log('Query single list with uid: ' + id);
-            users.retrieveUserByID(res, id);
-        
-            console.log("executed retrieve user by id")
-        
-        });
 
         // // route handler for default home page
         // router.get("/home", (req : Request, res : Response) => {
@@ -60,7 +47,7 @@ class App {
 
         this.expApp.use("/recipes", recipesRoute);
         this.expApp.use("/myrecipes", myRecipesRoute);
-        //this.expApp.use("/users", userRoute);
+        this.expApp.use("/users", userRoute);
     }
 }
 
